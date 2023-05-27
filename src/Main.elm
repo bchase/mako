c744c455d736ec161fd95b96dcd5da034e2e60cc
Brad Chase <brad@these8bits.com>
2023-05-27 11:08:03 +0900

    start basic app structure

--- src/Main.elm
+++ src/Main.elm
@@ -4,68 +4,213 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
 
 
 
 --- init / subs ---
 
-type alias Model =
-  { foo : String
-  }
 
-init : Flags -> (Model, Cmd Msg)
+type Lang
+    = JP
+    | EN
+
+
+type alias Model =
+    { view : View
+    , lang : Lang
+    }
+
+
+init : Flags -> ( Model, Cmd Msg )
 init flags =
-  cmds [] <| initModel flags
+    cmds [] <| initModel flags
+
 
 initModel : Flags -> Model
 initModel flags =
-  { foo = ""
-  }
+    { view = Disclaimer
+    , lang = JP
+    }
+
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.batch
-  [
-  ]
+subscriptions model =
+    Sub.batch
+        []
+
 
 
 --- update ---
 
-type Msg
-  = NoOp
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = case msg of
-  NoOp ->
-    just model
+type Msg
+    = NoOp
+    | SetLang Lang
+    | SetView View
+    | Post Msg
+
+
+type View
+    = Disclaimer
+    | BF
+    | IT
+
+
+type DisclaimerResp
+    = DisclaimerYes
+    | DisclaimerNo
+
+
+type BFResp
+    = BFYes
+    | BFNo
+
+
+type ITResp
+    = ITYes
+    | ITNo
+
+
+type Resp
+    = DisclaimerResp_ DisclaimerResp
+    | BFResp_ BFResp
+    | ITResp_ ITResp
+
+
+redirect : Resp -> View
+redirect resp =
+    case resp of
+        DisclaimerResp_ DisclaimerYes ->
+            Disclaimer
+
+        DisclaimerResp_ DisclaimerNo ->
+            Disclaimer
+
+        BFResp_ BFYes ->
+            Disclaimer
+
+        BFResp_ BFNo ->
+            Disclaimer
+
+        ITResp_ ITYes ->
+            Disclaimer
+
+        ITResp_ ITNo ->
+            Disclaimer
+
+
+copyFor : Lang -> View -> Html Msg
+copyFor lang v =
+    case lang of
+        JP ->
+            case v of
+                Disclaimer ->
+                    text ""
+
+                BF ->
+                    text ""
+
+                IT ->
+                    text ""
+
+        EN ->
+            case v of
+                Disclaimer ->
+                    text ""
+
+                BF ->
+                    text ""
+
+                IT ->
+                    text ""
+
+
+
+-- agree tos
+-- greeting
+-- ? authentication
+-- bf
+-- it
+--
+-- POST Msg for logs
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            just model
+
+        SetLang lang ->
+            just { model | lang = lang }
+
+        SetView view_ ->
+            just { model | view = view_ }
+
+        Post msg_ ->
+            model |> cmds [ log msg_ ]
+
+
+log : Msg -> Cmd Msg
+log msg =
+    let
+        body =
+            Http.stringBody "text/plain" (Debug.toString msg)
+
+        handler =
+            Http.expectString (always NoOp)
+    in
+    Http.post
+        { url = rootPath
+        , body = body
+        , expect = handler
+        }
+
+
+rootPath : String
+rootPath =
+    Debug.todo "rootPath"
 
 
 
 --- view ---
 
+
 view : Model -> Html Msg
 view model =
-  text ""
+    text ""
 
 
 
+-- viewDisclaimer : Html Msg
+-- viewDisclaimer =
 --- main ---
 
-type alias Flags = ()
+
+type alias Flags =
+    ()
+
 
 main : Program Flags Model Msg
-main = Browser.element
-  { init          = init
-  , update        = update
-  , subscriptions = subscriptions
-  , view          = view
-  }
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
 --- helpers ---
 
-just : Model -> (Model, Cmd Msg)
-just = cmds []
 
-cmds : List (Cmd Msg) -> Model -> (Model, Cmd Msg)
-cmds cs model = (model, Cmd.batch cs)
+just : Model -> ( Model, Cmd Msg )
+just =
+    cmds []
+
+
+cmds : List (Cmd Msg) -> Model -> ( Model, Cmd Msg )
+cmds cs model =
+    ( model, Cmd.batch cs )
